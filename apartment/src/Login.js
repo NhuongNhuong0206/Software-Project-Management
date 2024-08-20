784df8594cf7059a7ec64655494713d8e319c4e5
NguyenHoangNhi <2151053043nhi@ou.edu.vn>
2024-08-20 17:52:05 +0700

    Lập trình giao diện trang đăng nhập

---
+++ apartment/src/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <h1>Đăng nhập</h1>
+            <p>Chào mừng bạn đến với chung cư TNVV</p>
+            <form className="login-form">
+                <div className="input-group">
+                    <input type="text" placeholder="Tên đăng nhập" />
+                    <span className="input-icon">🥸</span>
+                </div>
+                <div className="input-group">
+                    <input type="password" placeholder="Password" />
+                    <span className="input-icon">👀</span>
+                </div>
+                <a href="/" className="forgot-password">Quên mật khẩu?</a>
+                <button type="submit" className="login-button">
+                    Đăng nhập
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
