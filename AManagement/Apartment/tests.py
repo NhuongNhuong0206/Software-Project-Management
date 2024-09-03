d6a9f13dc73d40ed95bc94ec66699a4a83066793
yanghara <109479069+yanghara@users.noreply.github.com>
2024-09-03 22:53:20 +0700

    Kiểm thử đơn vị

--- AManagement/Apartment/tests.py
+++ AManagement/Apartment/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from webbrowser import get
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
+from rest_framework.authtoken.models import Token
+from .models import Letters, User, People
+from .views import LettersViewSet, InfoPeopleViewSet
+
+
+class InfoPeopleViewSetTest(TestCase):
+
+    def test_get_people(self):
+
+        people = People.objects.filter(sex__icontains="Nữ")
+
+        self.assertTrue(people.exists())
+
+        self.assertEqual(people.count(), 1)
+
+
+class LettersViewSetTest(TestCase):
+    def setUp(self):
+
+        # Tạo token cho người dùng
+        self.client = APIClient()
+        self.client.force_authenticate(user=4)
+
+    def test_get_letters(self):
+        # Gửi yêu cầu GET đến API
+        response = self.client.get('/letter/')
+
+        # Kiểm tra status code
+        self.assertEqual(response.status_code, 200)
+
+        # Kiểm tra số lượng phản ánh trả về
+        self.assertEqual(len(response.data), 2)
+
+        # Kiểm tra nội dung của phản ánh
+        self.assertEqual(response.data[0]['content'], 'Complaint 1')
+
